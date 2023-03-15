a9b2b2a9ff6efe426ed6093814888060384b4711
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-03-15 12:36:49 +0530

    refactor: remove  struct

--- ext_models/message.go
+++
@@ -1,7 +0,0 @@
-package ext_models
-
-// type Message struct {
-// 	_msgpack struct{}    `msgpack:",as_array"`
-// 	Method   string      `msgpack:"method"`
-// 	Args     interface{} `msgpack:"args"`
-// }
