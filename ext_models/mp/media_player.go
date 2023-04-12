a95403e5785382fbbc70c83889438c235a762ac6
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-04-12 11:42:18 +0530

    feat: Add relevant todo

--- ext_models/mp/media_player.go
+++ ext_models/mp/media_player.go
@@ -1,5 +1,7 @@
 package ext_mp
 
+// TODO: Change from using index values, to player names. That is more reliable
+// and more useful as a unique MPRIS value.
 type MPlayerPlay struct {
 	_msgpack    struct{} `msgpack:",as_array"`
 	PlayerIndex int
